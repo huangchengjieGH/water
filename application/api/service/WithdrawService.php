5e906f59731648d2ee0462401791d577e43258d1
黄成杰 <huangchengjie@huangchengjiedeMacBook-Pro.local>
2018-05-15 22:31:29 +0800

    update

--- application/api/service/WithdrawService.php
+++ application/api/service/WithdrawService.php
@@ -62,7 +62,7 @@ class WithdrawService{
         $encode_data = '';
         $split = str_split($data, 100);// 1024bit && OPENSSL_PKCS1_PADDING  不大于117即可
         foreach ($split as $part) {
-            $isOkay = openssl_public_encrypt($part, $en_data, $pu_key);
+            $isOkay = openssl_public_encrypt($part, $en_data, $pu_key,OPENSSL_PKCS1_OAEP_PADDING);
             if(!$isOkay){
                 return false;
             }
